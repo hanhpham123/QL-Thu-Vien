a47d227f5a9c2d6bb1e23569dd4ffe690d05a0a9
=bnga1410 <=bnga14101999@gmail.com>
2020-02-15 21:19:47 +0700

    Add project files.

---
+++ TTN_QLTV/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTN_QLTV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void mượnSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chiTiếtMượnSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void độcGiảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void nhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void tácGiảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void thểLoạiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void đổiMậtKhẩuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TTN_QLTV/frmDangNhap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace TTN_QLTV
+{
+    public partial class frmDangNhap : Form
+    {
+
+        string Strcon = @"Data Source=NGABUI\SQLEXPRESS01;Initial Catalog=QLTV_LOAN;Integrated Security=True";
+        SqlConnection conn;
+        SqlCommand cmd;
+
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btdangnhap_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btthoat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ckhienthi_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
